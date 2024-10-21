22e0cbd8902cde8f6dcc020b7fd683840b627cfc
Timamov <timurmov25@gmail.com>
2024-10-21 20:08:55 +0300

    21.10.2024

--- hw13.py
+++ hw13.py
@@ -3,20 +3,30 @@ import requests
 import jinja2
 
 
-url = 'https://script.googleusercontent.com/macros/echo?user_content_key=xHhBmCrj0B0DUKKFaByiPuDHtr41kEGhDUUspvtoxncJOcDpd-RiqU18imgPoShWW5PvOTyuPtVCxVFZiwhCYLma3BP4S5jAm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnNQGuJX8AREF9xUXhEgDIbPCZNp-XoumTYjFLBdleLR5GrTEPkKmZ5iOp2D5pmbqc3697XehTrJlc4nTBGwdfvmm6JqsHcHsmw&lib=M76dUUs45UzOi5zse8KSKohj_FdURCWWA'
+url = 'https://script.google.com/macros/s/AKfycbzqPlSA48GBjMpLu_Z4CfiOgD3-t3cHCODyQ0OacRoi5PBnXL1l6qyJamG0hV9Yw7w/exec'
+
 response = requests.get(url)
 response_json = response.json()
 data: list[dict] = response_json['data']
 
-
 cost = 0
 for info in data:
-    if info['Is it poisonous'] == True:
-        cost += info['Price for keeping on eye']
+    if info['Is_it_poisonous'] == True:
+        cost += info['Price_for_keeping_on_eye']
         print(cost)
-    elif info['Where did it come from'] == 'Africa':
+    elif info['Where_did_it_come_from'] == 'Africa':
         print(info)
 
+most_expensive_animal = None
+highest_cost = 0
+for animal in data:
+    if animal['Price_for_keeping_on_eye'] > highest_cost:
+        most_expensive_animal = animal
+        highest_cost = animal['Price_for_keeping_on_eye']
+
+if most_expensive_animal:
+    print(most_expensive_animal)
+    print(highest_cost)
 
 with open('hw13.html', encoding='utf-8') as file:
     zoo_info = file.read()
@@ -27,12 +37,20 @@ def render_html(html_path: str, params: dict) -> str:
     template = template_env.get_template(html_path)
     output = template.render(params)
     return output
+pass
+params = {
+    'animal_name': most_expensive_animal['Animal_name'],
+    'animal_cost': most_expensive_animal['Price_for_keeping_on_eye']
+}
+
+output_html = render_html('hw13.html', params)
+with open('hw13.html', 'w', encoding='utf-8') as file:
+    file.write(output_html)
 
 
-for info in data:
-    send_email(
+ 
+send_email(
         ['timov31@ukr.net'],
-        render_html('hw13.html', info),
+        output_html,
         'mail subject',
     )
-pass
