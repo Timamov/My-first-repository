65db4059c27214e08584619049581e9c5a3bced3
Timamov <timurmov25@gmail.com>
2024-10-29 19:42:24 +0200

    28.10.2024

--- decorators.py
+++ decorators.py
@@ -2,15 +2,15 @@ import random
 def check_number_decorator(func):
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
-        if type(*args) is int:
-            modified_result = {'number': result + 10}
+        if type(result) is int:
+            modified_result = result + 10
         else:
-            modified_result = {'number': result}
+            modified_result = result
         return modified_result
     return wrapper
 
 @check_number_decorator
-def get_number(num):
-    return num
-number = 35
+def get_number(number):
+    return number
+number = 32
 print(get_number(number))
